f6fe93a670972a5af82020e14140b25319a28022
oerraoui <itsscarl2022@gmail.com>
2025-06-18 18:19:31 +0100

    Learn clone structs , loops , forEach

---
+++ mutability.js
@@ -0,0 +1,21 @@
+const person = {
+	name 	: 'Rick',
+	age 	: 77,
+	country : 'us',
+}
+
+const samePerson = {...person};
+const clone1 = {...person};
+const clone2 = {...person};
+
+Object.freeze(clone1)
+Object.freeze(clone1)
+
+//slice.push(person,clone1,clone2)
+
+//slice.forEhow to append ach(x =>  {
+//	console.log("constent :" , x )
+//})
+//console.log("content :" , slice);
+
+
