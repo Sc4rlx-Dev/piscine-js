1dee5a764939d5b3e45bdf25d7f1f4ca1c6a6857
oerroaui <itsscarl2022@gmail.com>
2025-06-18 12:58:03 +0100

    Learn how to declare vars | nested print values | datatypes

---
+++ primitives.js
@@ -0,0 +1,16 @@
+//declare vars
+const str = "hey js !"
+const num = 1234
+const bool = true
+const undef = undefined
+
+//print values
+
+console.log("str	: ", str , "Typeof : ", str, ")")
+console.log("num	: ", num , "Typeof : ", num, ")")
+console.log("bool	: ", bool, "Tyleof : ", bool, ")")
+console.log("undef	; ", undef,"Typeof : ", undef, ")")
+
+
+
+
