c80afdc3eb86c0489ac3a07735238004fce5a0ca
oerraoui <itsscarl2022@gmail.com>
2025-06-18 18:46:01 +0100

    Learn clone structs , loops , forEach | forgot freez

--- mutability.js
+++ mutability.js
@@ -10,9 +10,16 @@ const clone2 = {...person};
 
 
 
+
 //Object.assign(samePerson, person)
-//Object.freeze(clone1)
-//Object.freeze(clone1)
+Object.freeze(clone1.name)
+Object.freeze(clone1.age)
+Object.freeze(clone1.country)
+Object.freeze(clone2.name)
+Object.freeze(clone2.age)
+Object.freeze(clone2.country)
+Object.freeze(clone1)
+Object.freeze(clone2)
 //person.name = "oussama" ;
 //console.log(person.name  , samePerson.name , clone1.name , clone2.name );
 //slice.push(person,clone1,clone2)
