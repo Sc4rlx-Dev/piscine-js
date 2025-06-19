287fafe3a65c616b69dceed03d829d905a7d8c0a
oussama erraoui <oerraoui@noreply.learn.zone01oujda.ma>
2025-06-19 14:03:55 +0000

    fix shallow copy

--- mutability.js
+++ mutability.js
@@ -4,8 +4,6 @@ const clone1 = {...person};
 const clone2 = {...person};
 
 
-
-
 //Object.assign(samePerson, person)
 Object.freeze(clone1.name)
 Object.freeze(clone1.age)
@@ -15,12 +13,3 @@ Object.freeze(clone2.age)
 Object.freeze(clone2.country)
 Object.freeze(clone1)
 Object.freeze(clone2)
-//person.name = "oussama" ;
-//console.log(person.name  , samePerson.name , clone1.name , clone2.name );
-//slice.push(person,clone1,clone2)
-//slice.forEhow to append ach(x =>  {
-//	console.log("constent :" , x )
-//})
-//console.log("content :" , slice);
-
-
