6add4e4f5d1ac4550678522ba1ba1366f703a936
oussama erraoui <oerraoui@noreply.learn.zone01oujda.ma>
2025-06-19 14:41:24 +0000

    add typecasting to string

--- concat-str.js
+++ concat-str.js
@@ -1,7 +1,7 @@
 function concatStr(str1 , str2 ){
 
-    const concated = str1.concat(str2);
+    const concated = String(str1) + String(str2);
 return concated
 }
-
-// console.log("line : ", concatStr("text1","text2"));
+// 
+// console.log("line : ", concatStr(1,2));
