11452705bd8f51b37d18240265a56d416cff73c7
oerraoui <itsscarl2022@gmail.com>
2025-06-20 17:26:03 +0100

    Recode repeat() | Learn loops : for , while , do while

---
+++ repeat.js
@@ -0,0 +1,23 @@
+function repeat(str,n){
+    count = 0
+    res = ""
+    while(count < n){
+        res = res + str
+        // console.log("test")
+        count++
+    }
+return res
+}
+
+// console.log(repeat("string|", 2));
+
+// const str = "string "
+// console.log(`this is an test of how many should be reapeted =>${str.repeat(2)}`);
+
+
+// let count = 0 
+
+// while (count < 3 ){
+//     console.log(count)
+//     count++
+// }
