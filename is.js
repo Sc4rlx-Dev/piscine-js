8fd8bcf08a9f1ac3891446e1ec4cb1ce253b3aba
oerraoui <itsscarl2022@gmail.com>
2025-06-20 15:54:31 +0100

    object of functions | remove declaration methode , push funcs

--- is.js
+++ is.js
@@ -12,17 +12,18 @@
 //     falsy : (value) => { return (!is.truthy(value))} ,
 // };
 
-// const is = {}
-    is.num = (value) => { let res = !isNaN(value); return res };
-    is.nan = (value) => { let res = isNaN(value); return res };
-    is.str = (value) => { return typeof (value) === 'string' ? true : false; };
-    is.bool = (value) => { let res = typeof (value) === 'boolean'; return res };
-    is.undef = (value) => { let res = typeof (value) === 'undefined'; return res };
-    is.def = (value) => { let res = typeof (value) !== 'undefined'; return res };
-    is.arr = (value) => { let res = Array.isArray(value); return res };
-    is.obj = (value) => { let res = typeof (value) === 'object'; return res };
-    is.fun = (value) => { let res = typeof (value) === 'function'; return res };
-    is.truthy = (value) => { return (value === false || value == 0 || value == '' || value == null || is.undef(value) === true || is.nan(value) === true ); };
-    is.falsy = (value) => { return (!is.truthy(value)) };
+// const is = {};
+
+is.num = (value) => { let res = !isNaN(value); return res };
+is.nan = (value) => { let res = isNaN(value); return res };
+is.str = (value) => { return typeof (value) === 'string' ? true : false; };
+is.bool = (value) => { let res = typeof (value) === 'boolean'; return res };
+is.undef = (value) => { let res = typeof (value) === 'undefined'; return res };
+is.def = (value) => { let res = typeof (value) !== 'undefined'; return res };
+is.arr = (value) => { let res = Array.isArray(value); return res };
+is.obj = (value) => { let res = typeof (value) === 'object' && !Array.isArray(value) && value !== null; return res };
+is.fun = (value) => { let res = typeof (value) === 'function'; return res };
+is.truthy = (value) => { return !(value === false || value == 0 || value == '' || value == null || is.undef(value) === true || is.nan(value) === true); };
+is.falsy = (value) => { return !is.truthy(value); };
 
 // console.log(is.falsy(0));
