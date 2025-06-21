b304f2f7889b7281ffd6cfb676c298e7463da955
oerroaui <itsscarl2022@gmail.com>
2025-06-21 15:35:59 +0100

    refactor slice function to simplify end index handling and clean up return statement

--- slicer.js
+++ slicer.js
@@ -3,27 +3,24 @@
 // console.log(animals.slice(2));
 // Expected output: Array ["camel", "duck", "elephant"]
 
+
 function slice(arr, start, n) {
     if (typeof arr === 'string') {
         arr = arr.split('');
     }
 
-    let end = n;
-    if (end === undefined) {
-        end = arr.length;
-    }
-
+    let end = n || arr.length;
     let newArr = [];
 
     for (let i = start; i < end; i++) {
         newArr.push(arr[i]);
     }
 
-    if (arr) {
-        return newArr;
-    }
-    return newArr;
+    return arr ? newArr : newArr;
 }
 
+const animals = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(animals.slice(2));
+
 // console.log(slice('abcdef', 2)); 
-// console.log("Test :" ,  slice(["hello", "world","js","scarlx"] , 0 , 3));
+// console.log("Test :" ,  slice(["hello", "world","js","scarlx"] , 3));
