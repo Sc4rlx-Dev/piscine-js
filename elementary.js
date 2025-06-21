8ee9fc74eeacdfbe46468f903ad5da90af8d4bf8
oerraoui <itsscarl2022@gmail.com>
2025-06-21 11:25:37 +0100

    math operators implemetations js | fix 0 case

--- elementary.js
+++ elementary.js
@@ -1,31 +1,47 @@
 function multiply(a , b){
-    let result = 0, isNegative = false;
-    if (b < 0) { isNegative = true; b = -b; }
-    for (let i = 0; i < b; i++) { result += a; }
-    return isNegative ? -result : result;
-}
+    let res = 0;
 
+    while (b !== 0) {
+        if (b < 0) {
+            res -= a;
+            b++;
+        } else {
+            res += a;
+            b--;
+        }
+    }
+    return res;
+}
 
 // console.log(multiply(123,-22))
 
 
 function divide(c, d) {
+    if (d === 0) {
+        return 0;
+    }
+    let negative = (c < 0) !== (d < 0);
     c = Math.abs(c);
+    d = Math.abs(d);
     let quotient = 0;
     while (c >= d) {
         c -= d;
         quotient++;
     }
-    return quotient;
-}   
-// console.log(divide(5 , 9));
+    return negative ? -quotient : quotient;
+}
+// console.log(divide(0 , 0));
 
 
 function modulo(e , f){
-    let result = e;
-    while (result >= f) {
-        result -= f;
+    if (f === 0) {
+        return 0;
     }
-    return result;
+    let result = Math.abs(e);
+    let divisor = Math.abs(f);
+    while (result >= divisor) {
+        result -= divisor;
+    }
+    return e < 0 ? -result : result;
 }
 // console.log(modulo(5, 2));
