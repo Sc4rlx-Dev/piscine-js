a49457b982c30e560f900ff0b10a1f36f6ccac15
oerroaui <itsscarl2022@gmail.com>
2025-06-22 12:44:26 +0100

    refactor: simplify trunc, floor, ceil, and round functions by improving logic by increasing steps

--- cut-corners.js
+++ cut-corners.js
@@ -1,19 +1,34 @@
-export function trunc(n) {
+function trunc(n) {
+  if (n === 0) {return 0};
+  let res = 0, step = 1;
+  
   if (n > 0) {
-    let i = 0;
-    while (i + 1 <= n) {
-      i = i + 1;
+    while (res + step <= n) {
+      step = step * 2;
     }
-    return i;
+    // console.log(res + step)
+    while (step >= 1) {
+      if (res + step <= n) {
+        res = res + step;
+      }
+      step = step / 2;
+    }
+  } else {
+    while (res - step >= n) {
+      step = step * 2;
+    }
+    
+    while (step >= 1) {
+      if (res - step >= n) {
+        res = res - step;
+      }
+      step = step / 2;
+    }
   }
-  let i = 0;
-  while (i - 1 >= n) {
-    i = i - 1;
-  }
-  return i;
+  return res;
 }
 
-export function floor(n) {
+function floor(n) {
   const tr = trunc(n);
   if (n < 0 && n !== tr) {
     return tr - 1;
@@ -21,17 +36,21 @@ export function floor(n) {
   return tr;
 }
 
-export function ceil(n) {
+function ceil(n) {
   const tr = trunc(n);
   if (n > 0 && n !== tr) {
     return tr + 1;
   }
   return tr;
 }
+// // nums.map(ceil), [4, -3, 3, -2, 0]
 
-export function round(n) {
-  if (n < 0 && n + 0.5 === 0) {
-    return 0;
-  }
+function round(n) {
   return floor(n + 0.5);
 }
+
+// // const nums = [3.7, -3.7, 3.1, -3.1]
+// // console.log(nums.map(round))
+// // console.log(nums.map(floor))
+// // console.log(nums.map(trunc))
+// // console.log(nums.map(ceil))
