7d766e98a33793bd60401dc7fdbc113b5be490e6
oerroaui <itsscarl2022@gmail.com>
2025-06-23 11:46:01 +0100

    add: create its-a-match.js with regex patterns for matching "hi"

---
+++ its-a-match.js
@@ -0,0 +1,14 @@
+const normal = /hi/
+
+const begin = /^hi/
+
+const end = /hi$/
+
+const beginEnd = /^hi$/
+
+
+// console.log(normal.test("hi there"))
+// console.log(begin.test("hi there")) 
+// console.log(end.test("hi sdsds"))
+// console.log(beginEnd.test("hi")) 
+// console.log(beginEnd.test("hi there")) 
