1cc5d2a1963ff166bda3029c1c5d35214d8e3c45
oerroaui <itsscarl2022@gmail.com>
2025-06-23 13:58:56 +0100

    add: implement sameAmount function to compare occurrences of two regex patterns in a string

---
+++ same-amount.js
@@ -0,0 +1,35 @@
+
+
+
+function sameAmount(str , gtx , rtx){
+
+let cart_graphic = new RegExp(gtx, 'g')
+let cart_graphic2 = new RegExp(rtx, 'g')
+
+ 
+    // res = gtx.test(str)
+let check =  str.match(cart_graphic) 
+if ( check == null) {
+
+    check = []
+}
+let check2 =  str.match(cart_graphic2) 
+if ( check2 == null) {
+
+    check2 = []
+}
+
+    // res = rtx.test(str)
+    // console.log()
+    // console.log(rtx.test(str))
+    // console.log(gtx.test(str))
+
+return check.length == check2.length
+    // return gtx.test(str) && rtx.test(str)
+}
+ 
+
+
+
+// console.log(
+// sameAmount("string", /h/, /w/))
