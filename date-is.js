b5f600b5147ec7de143016d445dc1d02c6c0d394
oerraoui <itsscarl2022@gmail.com>
2025-06-26 10:39:48 +0100

    add date validation functions | isValid, isAfter, isBefore, isFuture, isPast

---
+++ date-is.js
@@ -0,0 +1,36 @@
+function isValid(datee){
+
+    if(!(datee instanceof Date) && !isNaN(datee) ){
+        return false
+    }
+return true
+}
+
+
+function isAfter(date1 , date2){
+    return Date.parse(date1) > Date.parse(date2) 
+}
+function isBefore(date1 , date2){
+    if(Date.parse(date1) < Date.parse(date2) ){
+        return true
+    }
+return false
+}
+
+function isFuture(datee){
+    if (isValid(datee) && isAfter(datee , Date.now()))   {
+        return true
+    }
+return false
+}
+
+function isPast(datee){
+    if(isValid(datee) && isBefore(datee , Date.now() )){
+        return true
+    }
+return false
+}
+
+
+// var result = isFuture(new Date(2014, 11, 31))
+// console.log(result)
