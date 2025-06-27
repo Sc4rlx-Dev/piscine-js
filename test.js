8294dbb61a5c904bb51abb720ca09abdbde71843
oerraoui <itsscarl2022@gmail.com>
2025-06-27 18:24:26 +0100

    Learn add functions using map to extract cities, format states, convert and clean temperatures | learn arrow fn , callbackfn

--- test.js
+++ test.js
@@ -1,3 +1,90 @@
 // console.log(new Date().toString()); // "Sat Sep 13 275760 00:00:00 GMT+0000 (Coordinated Universal Time)"
 
 // console.log(new Date().)
+
+
+// const map1 = new Map();
+
+// map1.set("a", 1);
+// map1.set("b", 2);
+// map1.set("c", 3);
+
+// console.log(map1.keys(0));
+
+function trimTemp(arr){
+
+    const res = arr.map( i => ({
+        city        : i.city , 
+        temperature : i.temperature.trim().replaceAll(' ', '') ,
+    }) )
+
+return res
+
+}
+
+
+console.log(
+trimTemp(
+[
+  { city: 'Los Angeles', temperature: '  101 °F   ' },
+  { city: 'San Francisco', temperature: ' 84 ° F   ' },
+]) 
+)
+
+
+
+// console.log(fahrenheitToCelsius(['68', '32']));
+
+// trimTemp(
+// [
+//   { city: 'Los Angeles', temperature: '  101 °F   ' },
+//   { city: 'San Francisco', temperature: ' 84 ° F   ' },
+// ]) /* -> [
+//   { city: 'Los Angeles', temperature: '101°F' },
+//   { city: 'San Francisco', temperature: '84°F' },
+// ] */
+
+
+// function trimTemp(arr3){
+//   // let item = ({ city, temperature })
+//     const result = arr3.map(item => ({ city: item.city, temperature: item.temperature.trim().replace(/\s+/g, '') }));
+// return result
+// }
+
+// console.log(trimTemp(
+//   [
+//     { city: 'Los Angeles', temperature: '  101 °F   ' },
+//     { city: 'San Francisco', temperature: ' 84 ° F   ' },
+//   ]))
+
+
+//       // console.log("test :",typeof(w))
+//         if(t.includes(w) === false ){
+//           res.push(t.trim())
+//         }
+
+//         if(t.includes(w)) {
+//           // console.log(typeof(t[i]))
+//           const cleaned = t.replaceAll(' ', '');
+//           res.push(cleaned);
+//           i++
+//           res.push(t[i].trim())
+//         }
+
+//     return res.join('')
+//     }
+//   const trim = arr3.map(t)
+// return trim
+
+
+
+
+// const array1 = [1, 4, 9, 16]
+
+// const cf = (v) => v + 1
+
+// Pass a function to map
+// const map1 = array1.map((test) => test * 2 );
+
+// console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
---
+++ using-map.js
@@ -0,0 +1,66 @@
+// citiesOnly([
+//   {
+//     city: 'Los Angeles',
+//     temperature: '  101 °F   ',
+//   },
+//   {
+//     city: 'San Francisco',
+//     temperature: ' 84 ° F   ',
+//   },
+// ]) // -> ['Los Angeles', 'San Francisco']
+
+
+function upperCasingStates(arr){
+const mapp = arr.map((t) => t[0].toUpperCase() + t.slice(1).toLowerCase());
+return mapp
+}
+
+// console.log(upperCasingStates(['alabama', 'new jersey']));
+
+function fahrenheitToCelsius(arr2){
+const degree = arr2.map((c) => (c - 32 )/ 1.8 )
+return degree
+}
+
+
+
+function trimTemp(arr){
+    const res = arr.map( i => ({
+        city        : i.city , 
+        temperature : i.temperature.trim().replaceAll(' ', '') ,
+    }) )
+
+return res
+
+}
+
+// console.log(
+// trimTemp(
+// [
+//   { city: 'Los Angeles', temperature: '  101 °F   ' },
+//   { city: 'San Francisco', temperature: ' 84 ° F   ' },
+// ]) 
+// )
+
+
+function tempForecasts (arr4){
+const res = arr4.map(i => ({
+  city: upperCasingStates([i.city]),
+  temperature: Math.round(fahrenheitToCelsius([parseInt(i.temperature)])) + '°Celsius',
+  state: upperCasingStates([i.state]),
+  region: i.region,
+}));
+return res.map(i => `${i.temperature} in ${i.city}, ${i.state}`);
+}
+
+
+console.log(
+  tempForecasts([
+    {
+      city: 'Pasadena',
+      temperature: ' 101 °F',
+      state: 'california',
+      region: 'West',
+    },
+  ])
+); // -> ['38°Celsius in Pasadena, California']
