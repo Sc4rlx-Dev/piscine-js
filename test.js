9019a599ef2bfe4457bd9b4aeae95a7234f81e61
oerraoui <itsscarl2022@gmail.com>
2025-06-28 16:25:03 +0100

    Learn foreach() | async , await ..

--- test.js
+++ test.js
@@ -195,3 +195,27 @@
 
 // console.log(array.some(even));
 // // Expected output: true
+
+// const array1 = ["a", "b", "c"];
+
+// array1.forEach((element) => console.log(element));
+
+// // Expected output: "a"
+// // Expected output: "b"
+// // Expected output: "c"
+
+
+
+// const ratings = [5, 4, 5];
+// let sum = 0;
+
+// const sumFunction = async (a, b) => a + b;
+
+// ratings.forEach(async (rating) => {
+//   sum = await sumFunction(sum, rating);
+// });
+
+// console.log(sum);
+// // Naively expected output: 14
+// // Actual output: 0
+
