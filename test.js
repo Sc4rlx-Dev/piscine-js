2691d0b5b16c4af0e4bfacd3b0fa0e23d243b896
oerraoui <itsscarl2022@gmail.com>
2025-06-28 17:10:04 +0100

    Make map() , flatMap() |

--- test.js
+++ test.js
@@ -219,3 +219,9 @@
 // // Naively expected output: 14
 // // Actual output: 0
 
+// const arr1 = [1, 2, 1]
+
+// const result = arr1.flatMap((num) => (num === 2 ? [2, 10 , 5] : 1))
+
+// console.log(result)
+// Expected output: Array [1, 2, 2, 1]
