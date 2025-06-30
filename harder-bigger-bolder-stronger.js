57a3f6f125fb269576d938fc9b9df7a8456b9385
oerraoui <itsscarl2022@gmail.com>
2025-06-30 12:23:59 +0100

    learn styling divs | fix size gradient pixels

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -3,13 +3,14 @@ export const generateLetters = () => {
     let i = 0
     const buf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const len = buf.length
+    let px = 10
 
      while(i < 120){
-
+        px += 1
         const nwdiv = document.createElement("div")
         document.body.append(nwdiv)
         nwdiv.append(buf.charAt(Math.floor(Math.random() * len)))
-        nwdiv.style.fontSize = "130px"
+        nwdiv.style.fontSize = `${px}px`
         if(i <= 40) { nwdiv.style.fontWeight = 300} else if( i <= 80) { nwdiv.style.fontWeight = 400 } else { nwdiv.style.fontWeight = 600 }
         i++
      }
