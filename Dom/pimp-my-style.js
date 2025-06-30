88f7aa3e4c11534736426a0f2e678c14565b585d
oerraoui <itsscarl2022@gmail.com>
2025-06-30 15:47:05 +0100

    learn classlist add(), remove(), merge with eventListner

---
+++ Dom/pimp-my-style.js
@@ -0,0 +1,32 @@
+import { styles } from "./pimp-my-style.data.js"
+var i = 0
+let len = styles.length
+let flag = true
+
+export const pimp = () => {
+    const btn = document.querySelector("button")
+    if (!btn) { return }
+
+    if(flag) {
+        btn.classList.remove("unpimp")
+        btn.classList.add(styles[i])
+        i++
+        if(i == len) {
+            flag = false
+        }
+    } else{
+        btn.classList.remove(styles[i])
+        btn.classList.add("unpimp")
+        i--
+
+        if(i == 0) {
+        btn.classList.remove("unpimp")
+        flag = true
+        }
+    }
+}
+
+
+
+
+
