76f0e0db3c62231a9d6453a11a2aaaa0cfba20ec
oerraoui <itsscarl2022@gmail.com>
2025-07-02 15:32:50 +0100

    refactor pick and omit functions for consistency and improved readability

--- pick-omit.js
+++ pick-omit.js
@@ -1,30 +1,35 @@
-export const pick = (obj , str) => {
-    let newobj = new Object()
-    
-    Object.entries(obj).forEach(([key,value]) => {
-        if(key == str) {
-            newobj[key]= value
+export const pick = (obj, str) => {
+    let newobj = {}
+    if (typeof str !== 'string') {
+        str = [str]
+    }
+
+    Object.entries(obj).forEach(([key, value]) => {
+        if (str.includes(key)) {
+            newobj[key] = value;
+        }
+    });
+    return newobj
+}
+
+export const omit = (obj, str) => {
+    let newobj = {};
+
+    if (typeof str !== 'string') {
+        str = [str]
+    }
+
+    Object.entries(obj).forEach(([key, value]) => {
+        if (!str.includes(key)) {
+            newobj[key] = value;
         }
     })
-return newobj
+    return newobj
 }
 
 
-export const omit = () => {
-let newobj = new Object()
-    
-    Object.entries(obj).forEach(([key,value]) => {
-        if(key != str) {
-            newobj[key]= value
-        }
-    })
-return newobj
-    
-}
 
+const obj = { a: 1, b: 2, c: 3 }
+const str = 'a'
 
-
-// const obj = { a: 1, b: 2, c: 3 }
-// const str = 'a'
-
-// console.log(omit(obj, str));
+console.log(omit(obj, str))
