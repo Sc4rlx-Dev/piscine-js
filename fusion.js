93b74acb8fd62b7be10f50c2814ad4ff9a04b4aa
Oussama Erraoui <oerraoui>
2025-07-03 13:50:25 +0100

    fusion

--- fusion.js
+++ fusion.js
@@ -1,23 +1,26 @@
-export const fusion = (t , s ) => {
+export const fusion = (target, source) => {
+    const result = { ...target }
 
-// console.log(fusion({ arr: [1, "2"] }, { arr: [2] }))
-// // -> { arr: [ 1, '2', 2 ] }
-    let res = Object.assign(t)
-
-    for (const key in s) {
-        if (Array.isArray(s[key]) && Array.isArray(res[key])) {
-            res[key] = [...res[key], ...s[key]]
-        } else if (typeof s[key] === 'string' && typeof res[key] === 'string') {
-            res[key] = `${res[key]} ${s[key]}`
-        } else if (typeof s[key] === 'number' && typeof res[key] === 'number' ) {
-            res[key] = res[key] + s[key]
-        } else if (typeof s[key] === 'object' && typeof res[key] === 'object') {
-            // res[key] = s[key]
-            // res[key] = fusion(res[key] , s[key])
+    for (const key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+            if (Array.isArray(result[key]) && Array.isArray(source[key])) {
+                result[key] = [...result[key], ...source[key]];
+            } else if (
+                typeof result[key] === 'object' && result[key] !== null && !Array.isArray(result[key]) &&
+                typeof source[key] === 'object' && source[key] !== null && !Array.isArray(source[key])
+            ) {
+                result[key] = fusion(result[key], source[key]);
+            } else if (typeof result[key] === 'number' && typeof source[key] === 'number') {
+                result[key] = result[key] + source[key];
+            } else if (typeof result[key] === 'string' && typeof source[key] === 'string') {
+                result[key] = `${result[key]} ${source[key]}`;
+            } else {
+                result[key] = source[key];
+            }
         }
     }
 
-return res
+    return result;
 }
 
 // console.log(fusion({ arr: [1, 2 , 3] }, { arr: [4] }))
