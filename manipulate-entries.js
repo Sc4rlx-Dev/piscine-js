cc1574576939e523cf2c729d4695caf21c9004b8
oerraoui <itsscarl2022@gmail.com>
2025-07-04 16:41:18 +0100

    refactor totalCalories function to return a fixed decimal value and restore nutritionDB for clarity

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -48,7 +48,7 @@ export const totalCalories = (ob) => {
         // console.log(itmcal)
         return total + itmcal
     },0)
-    return al
+    return +al.toFixed(2)
 }
 
 
@@ -107,18 +107,18 @@ export const cartTotal = (cart) => {
 
 // small database with nutrition facts, per 100 grams
 // prettier-ignore
-// const nutritionDB = {
-//   tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
-//   vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
-//   oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
-//   onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
-//   garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
-//   paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
-//   sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
-//   orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
-// }
+const nutritionDB = {
+  tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+  vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+  oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+  onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+  garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+  paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+  sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+  orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+}
 
-// const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
+const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
 
 // console.log('Total calories:')
 // console.log(totalCalories(groceriesCart))
@@ -126,3 +126,4 @@ export const cartTotal = (cart) => {
 // console.log(lowCarbs(groceriesCart))
 // console.log('Total cart nutritional facts:')
 // console.log(cartTotal(groceriesCart))
+console.log(totalCalories(groceriesCart), 1921.4)
