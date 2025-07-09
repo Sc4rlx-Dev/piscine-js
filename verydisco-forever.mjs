1cb4c2016c32080c1efb850ab01b256b5c640455
oerraoui <itsscarl2022@gmail.com>
2025-07-09 18:43:55 +0100

    fix: ensure output is written to file when no input is provided

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -4,15 +4,14 @@ const input = process.argv[2]
 
 function Disco(word) {
     const m = Math.ceil(word.length / 2)
-    const f = word.slice(0 , m)
+    const f = word.slice(0, m)
     const s = word.slice(m)
     return s + f
 }
 
-if (!input) { console.log("🕺🏼") ; process.exit(1) }
+if (!input) { writeFileSync('verydisco-forever.txt', '🕺🏼') ; process.exit(1) }
+
 const words = input.split(" ")
 const result = words.map(Disco).join(" ")
 
-console.log(result)
-
-writeFileSync('verydisco-forever.txt', result + '\n')
+writeFileSync('verydisco-forever.txt', result)
