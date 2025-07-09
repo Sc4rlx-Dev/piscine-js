d72da4d125bece260d17cae4f454e697cedc3557
oerraoui <itsscarl2022@gmail.com>
2025-07-09 18:21:08 +0100

    refactor: simplify input handling and improve formatting in verydisco.mjs

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,17 +1,15 @@
-const input = process.argv.slice(2).join(" ")
+const input = process.argv[2]
 
 function Disco(word) {
     const m = Math.ceil(word.length / 2)
-    const f = word.slice(0, m)
+    const f = word.slice(0 , m)
     const s = word.slice(m)
+    // console.log(s + f)
     return s + f
 }
 
-if (!input) {
-    console.log("🕺🏼")
-    process.exit(1)
-}
-
+if (!input) { console.log("🕺🏼") ; process.exit(1) }
 const words = input.split(" ")
 const result = words.map(Disco).join(" ")
+
 console.log(result)
