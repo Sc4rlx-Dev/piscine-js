b7652e0e1e1f47e1b05a75f314db0f3f3a35fcf4
oerraoui <itsscarl2022@gmail.com>
2025-07-10 12:39:53 +0100

    fix manipulate data handling | fix flooring

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,16 +1,17 @@
 import fs from 'node:fs'
 const arg1 = process.argv[2]
 
-function rev(p) {
-    const words = p.split(' ')
-    const revwd = words
-    .map(w => w.split('').reverse().join(''))
-    .reverse()
-return revwd.join(' ')
+function rev(word) {
+    const m = Math.floor(word.length / 2) ; const p1 = word.slice(0, m); const p2 = word.slice(m) 
+    return p2 + p1
 }
 
-fs.readFile(arg1, 'utf8' ,(err ,data) => { if(err) { console.log(err) ; return }
-// console.log(data)
-const res = rev(data)
-console.log(res) 
+fs.readFile(arg1, 'utf8', (err, data) => { 
+    if(err) {  console.log(err) ; return }
+    
+    const result = data.trim().split(' ')
+        .map(word => rev(word))
+        .join(' ');
+    
+    console.log(result); 
 })
