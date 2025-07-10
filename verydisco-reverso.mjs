fcbd6ab82efb45c5186e45db825fed1736893cef
oerraoui <itsscarl2022@gmail.com>
2025-07-10 12:13:19 +0100

    manipulate data handling

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,3 +1,15 @@
 import fs from 'node:fs'
 const arg1 = process.argv[2]
-fs.readFile(arg1, 'utf8' ,(err ,data) => { if(err) { console.log(err) ; return  }; console.log(data) })
+
+function rev(p) { 
+    const m = Math.floor(p.length / 2)  
+    const p1 = p.slice(0 , m) 
+    const p2 = p.slice(m) 
+    return p2 + p1
+}
+
+fs.readFile(arg1, 'utf8' ,(err ,data) => { if(err) { console.log(err) ; return }
+// console.log(data)
+const res = rev(data)
+console.log(res) 
+})
