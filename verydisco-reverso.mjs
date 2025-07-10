e32c62ca6f8bad5354e7c7652880eab288b3c438
oerraoui <itsscarl2022@gmail.com>
2025-07-10 12:28:50 +0100

    fix manipulate data handling | reverse handling

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,11 +1,12 @@
 import fs from 'node:fs'
 const arg1 = process.argv[2]
 
-function rev(p) { 
-    const m = Math.floor(p.length / 2)  
-    const p1 = p.slice(0 , m) 
-    const p2 = p.slice(m) 
-    return p2 + p1
+function rev(p) {
+    const words = p.split(' ')
+    const revwd = words
+    .map(w => w.split('').reverse().join(''))
+    .reverse()
+return revwd.join(' ')
 }
 
 fs.readFile(arg1, 'utf8' ,(err ,data) => { if(err) { console.log(err) ; return }
